2cdcdf238f8809fd69a06fa86eabc3bb9e43cca6
Adam Soltys <asoltys@gmail.com>
2021-12-02 11:20:52 -0800

    metadata fixes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -62,7 +62,9 @@
 
 <svelte:window bind:scrollY={y} />
 
+{#if !($page.path.includes('/a/') && $page.path.split('/').length === 3)}
 <Head metadata={branding.meta} />
+{/if}
 
 <Snack />
 <div class="bg-brand">
