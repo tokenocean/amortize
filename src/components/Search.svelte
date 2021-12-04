941aff6fcdb75ab499db0cf48483ae437e58a34d
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2021-12-03 23:41:48 -0700

    Update styles

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -72,7 +72,7 @@
   <form on:submit|preventDefault={submit}>
     <div class="flex">
       <input
-        class="lg:w-1/3 border-0 border-b-2 rounded border-lightblue bg-greyOne text-greyTwo"
+        class="lg:w-1/3 border-0 border-b-2 rounded border-lightblue bg-blueberry text-greyOne"
         placeholder="Search..."
         on:input={({ target: { value } }) => debounce(value)} />
       <div class="my-auto ml-2">
